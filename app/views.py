8f0fe674b4f94b896b1439063d45069b9e08bdff
zhouyiran <djsolar1999@gmail.com>
2016-05-15 22:40:51 +0800

    1.增加登录页面 2.增加用户信息查看页面 3.增加用户信息修改页面

--- app/views.py
+++ app/views.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 __author__ = 'zhouyiran'
-from flask import render_template, flash, redirect
-from app import app
-from .forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request, g, session
+from app import app, db, lm, oid
+from flask.ext.login import login_required, login_user, logout_user, current_user
+from .forms import LoginForm, EditForm
+from models import User, Post, ROLE_USER, ROLE_ADMIN
+import datetime
+
+
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'name': 'djsolar'}
+    user = g.user
     posts = [
         {
             'author': {'nickname': 'John'},
@@ -24,13 +42,73 @@ def index():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for openid = ' + str(form.openid.data) +
-              ', remember_me = ' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved ')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+        return render_template('edit.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User ' + nickname + ' is not found')
+        redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'The post test1'},
+        {'author': user, 'body': 'The post test2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == '':
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == '':
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
