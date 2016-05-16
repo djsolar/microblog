ef06a1d2f2af59546f8d3a5661521902c959b556
zhouyiran <djsolar1999@gmail.com>
2016-05-16 19:26:02 +0800

    数据库的迁移、升级、降级

--- app/models.py
+++ app/models.py
@@ -4,10 +4,23 @@ __author__ = 'zhouyiran'
 from app import db
 
 
+# 用户表
 class User(db.Model):
     id = db.Column(db.INTEGER, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+
+# 用户发表的文章
+class Post(db.Model):
+    id = db.Column(db.INTEGER, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.INTEGER, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' %(self.body)
