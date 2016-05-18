df4de6214fe888264415360840e627ec9df38894
zhouyiran <djsolar1999@gmail.com>
2016-05-18 17:23:30 +0800

    更改配置文件

--- config.py
+++ config.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'zhouyiran'
 import os
-
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
@@ -20,3 +18,17 @@ OPENID_PROVIDERS = [
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIN_PASSWORD = None
+
+ADMINS = ['you@example.com']
+
+# 每一页显示的博客数量
+POSTS_PER_PAGE = 3
+# 搜索数据库
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+# 搜索结果的数目
+MAX_SEARCH_RESULTS = 50
