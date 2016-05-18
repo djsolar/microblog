87f5dc38350d7e01da00fc30ac0fa5767d1304ce
zhouyiran <djsolar1999@gmail.com>
2016-05-18 17:23:08 +0800

    更改逻辑

--- app/models.py
+++ app/models.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 __author__ = 'zhouyiran'
-from app import db
+from app import db, app
+import sys
+import flask.ext.whooshalchemy as whooshalchemy
 from hashlib import md5
+
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
+
+
+followers = db.Table('followers',
+                     db.Column('follower_id', db.INTEGER, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.INTEGER, db.ForeignKey('user.id')))
 
 
 class User(db.Model):
@@ -16,11 +24,17 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
-    def avatar(self, size):
+    def avatar(self, size=10):
         return 'http://www.91feizhuliu.com/uploads/allimg/120428/2116322C5-7.jpg'
 
     def is_authenticated(self):
@@ -35,8 +49,39 @@ class User(db.Model):
     def get_id(self):
         return unicode(self.id)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).\
+            filter(followers.c.follower_id == self.id).\
+            order_by(Post.timestamp.desc())
+
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() is None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname=new_nickname).first() is None:
+                break;
+            version += 1;
+        return new_nickname
+
 
 class Post(db.Model):
+    __searchable__ = ['body']
     id = db.Column(db.INTEGER, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
@@ -44,3 +89,6 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+
+whooshalchemy.whoosh_index(app, Post)
--- app/views.py
+++ app/views.py
@@ -4,9 +4,10 @@ __author__ = 'zhouyiran'
 from flask import render_template, flash, redirect, url_for, request, g, session
 from app import app, db, lm, oid
 from flask.ext.login import login_required, login_user, logout_user, current_user
-from .forms import LoginForm, EditForm
+from .forms import LoginForm, EditForm, PostForm, SearchForm
 from models import User, Post, ROLE_USER, ROLE_ADMIN
-import datetime
+from datetime import datetime
+from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS
 
 
 @lm.user_loader
@@ -18,27 +19,26 @@ def load_user(user_id):
 def before_request():
     g.user = current_user
     if g.user.is_authenticated:
-        g.user.last_seen = datetime.datetime.utcnow()
+        g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
+        g.search_form = SearchForm()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index/<int:page>', methods=['GET', 'POST'])
 @login_required
-def index():
-    user = g.user
-    posts = [
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Jim'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='home', user=user, posts=posts)
+def index(page=1):
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, timestamp=datetime.utcnow(), author=g.user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now lived!')
+        return redirect(url_for('index'))
+    posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
+    return render_template('index.html', title='home', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -59,7 +59,7 @@ def login():
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
-    form = EditForm()
+    form = EditForm(g.user.nickname)
     if form.validate_on_submit():
         g.user.nickname = form.nickname.data
         g.user.about_me = form.about_me.data
@@ -73,6 +73,22 @@ def edit():
         return render_template('edit.html', form=form)
 
 
+@app.route('/search', methods=['POST'])
+@login_required
+def search():
+    if not g.search_form.validate_on_submit():
+        return redirect(url_for('index'))
+    return redirect(url_for('search_results', query=g.search_form.search.data))
+
+
+@app.route('/search_results/<query>')
+@login_required
+def search_results(query):
+    results = Post.query.whoosh_search(query, limit=MAX_SEARCH_RESULTS).all()
+    print results
+    return render_template('search_results.html', query=query, results=results)
+
+
 @app.route('/logout')
 def logout():
     logout_user()
@@ -80,17 +96,67 @@ def logout():
 
 
 @app.route('/user/<nickname>')
+@app.route('/user/<nickname>/<int:page>')
 @login_required
-def user(nickname):
+def user(nickname, page=1):
     user = User.query.filter_by(nickname=nickname).first()
     if user is None:
         flash('User ' + nickname + ' is not found')
         redirect(url_for('index'))
-    posts = [
-        {'author': user, 'body': 'The post test1'},
-        {'author': user, 'body': 'The post test2'}
-    ]
+    posts = user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('Your cant\'t follow yourself.')
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.follow(user)
+    if u is None:
+        flash('Cannot follow ' + nickname + '!')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You are now following ' + nickname + '!')
+    return redirect(url_for('user', nickname=nickname))
+
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User % not found.' % nickname)
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t unfollow youself.')
+        return redirect(url_for('user', nickname=nickname))
+
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow ' + nickname + '!')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following ' + nickname + '.')
+    return redirect(url_for('user', nickname=nickname))
+
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 @oid.after_login
@@ -103,8 +169,11 @@ def after_login(resp):
         nickname = resp.nickname
         if nickname is None or nickname == '':
             nickname = resp.email.split('@')[0]
+        nickname = User.make_unique_nickname(nickname)
         user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
+        db.session.commit()
+        db.session.add(user.follow(user))
         db.session.commit()
     remember_me = False
     if 'remember_me' in session:
